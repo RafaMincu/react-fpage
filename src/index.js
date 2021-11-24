a565437e45039e36c7ee35e46a64227204b4912d
Rafa <rafamincu@gmail.com>
2021-11-24 12:20:18 +0200

    prima mea componenta

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import reactDom from 'react-dom'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render (
+    <div>Prima componenta</div>,
+    document.getElementById('root')
+)
