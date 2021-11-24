facae50f35320530be1c0e779ac4a37aacfc3fe4
Rafa <rafamincu@gmail.com>
2021-11-24 12:31:10 +0200

    am creat prima mea componenta App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Prima componenta</h1>
+                <p>Aceasta este prima mea componenta</p>
+            </div>
+        )
+    }
+}
+
+export default App
