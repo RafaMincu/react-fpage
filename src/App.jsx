959eb129055c1e34c848704e8d8556d685522094
Rafa <rafamincu@gmail.com>
2021-11-24 13:17:35 +0200

    Am adaugat un input si afisez intr-un paragraf in timp real textul introdus in input

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,21 @@ class App extends Component {
         
         this.state = {
             msg: "Prima componenta",
-            txt: "Aceasta este prima mea componenta"
+            txt: "Aceasta este prima mea componenta",
+            txtInput: "Aici va aparea textul introdus in input"
+
         }
     }
 
+    changeTitle (e) {
+        this.setState({
+            txtInput: e.target.value
+        })
+    }
 
     changeTxt () {
         this.setState({
-            txt: 'Acesta este noul text'
+            txt: 'Am apasat butonul Change Text'
         })
     }
 
@@ -24,6 +31,8 @@ class App extends Component {
                 <h1>{ this.state.msg }</h1>
                 <p>{ this.state.txt }</p>  {/* folosesc sintaxa this.state pentru a prelua informatii din interiorul componentei */}
                 <p>{ this.props.message }</p> {/* folosesc sintaxa this.props pentru a prelua informatii din exteriorul componentei */}
+                <p>{ this.state.txtInput }</p>
+                <input type="text" onChange={(e) => this.changeTitle(e)}/>
                 <button onClick={() => this.changeTxt()}> 
                     Change Text
                 </button>
