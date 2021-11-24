a56f21b16fd217d0186e566dd611bc5508372865
Rafa <rafamincu@gmail.com>
2021-11-24 14:02:17 +0200

    functionalitate de adaugare taskuri

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,16 @@ class App extends Component {
         this.state = {
             msg: "Prima componenta",
             txt: "Aceasta este prima mea componenta",
-            txtInput: "Aici va aparea textul introdus in input"
-
+            txtInput: "Aici va aparea textul introdus in input",
+            task: '',
+            tasks: []
         }
     }
 
-    changeTitle (e) {
+    changeText (e) {
+
         this.setState({
+            task: e.target.value,
             txtInput: e.target.value
         })
     }
@@ -23,6 +26,33 @@ class App extends Component {
         this.setState({
             txt: 'Am apasat butonul Change Text'
         })
+       this.addTask()
+    }
+
+    addTask () {
+        let tasks = this.state.tasks
+        tasks.push(this.state.task)
+        this.setState({
+            tasks
+        })
+        console.log(this.state.tasks)
+    }
+
+    listTasks() {
+        let tasks = this.state.tasks
+        return (
+            <ul>
+                {
+                    tasks.map((val, index) => {
+                        return (
+                            <li key={index}>
+                                    { val }
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
     }
 
     render () {
@@ -32,10 +62,11 @@ class App extends Component {
                 <p>{ this.state.txt }</p>  {/* folosesc sintaxa this.state pentru a prelua informatii din interiorul componentei */}
                 <p>{ this.props.message }</p> {/* folosesc sintaxa this.props pentru a prelua informatii din exteriorul componentei */}
                 <p>{ this.state.txtInput }</p>
-                <input type="text" onChange={(e) => this.changeTitle(e)}/>
+                <input type="text" onChange={(e) => this.changeText(e)}/>
                 <button onClick={() => this.changeTxt()}> 
-                    Change Text
+                    Add Task
                 </button>
+                { this.listTasks() }
             </div>
         )
     }
